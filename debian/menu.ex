4529b1007953981c7ac86d304e6ea071a7411f2f
Daniel Kastl <daniel@georepublic.de>
2015-02-25 04:39:18 +0900

    removed unnecessary templates

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(osrm-backend):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="osrm-backend" command="/usr/bin/osrm-backend"
--- debian/osrm-backend.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the osrm-backend package
-#
-0 4	* * *	root	[ -x /usr/bin/osrm-backend_maintenance ] && /usr/bin/osrm-backend_maintenance
